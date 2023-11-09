f739fdd1854aa24cca1ba4186ee8ce85ace13926
Dilnoza <ataieva2005@gmail.con>
2023-11-09 16:09:39 +0300

    начало верстки

--- app/src/main/java/com/example/x_rates/MainActivity.kt
+++ app/src/main/java/com/example/x_rates/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.x_rates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.x_rates.databinding.ActivityMainBinding
@@ -16,33 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
-        //navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
         binding.bottomNavigationView.setupWithNavController(navController)
-
-
-        /*binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_baseline_assessment -> {
-                    replaceFragment(FavorableRateFragment())
-                    true
-                }
-
-                R.id.bottom_mdi_bank -> {
-                    replaceFragment(NbtRateFragment())
-                    true
-                }
-
-                else -> false
-            }
-        }
-        replaceFragment(FavorableRateFragment())
-
-    }
-    private fun replaceFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
-    */
     }
 }
